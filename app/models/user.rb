4c71060aac2ae5a310699cf74ed50b00f0df8119
traycers <name@mail.com>
2018-11-19 00:01:48 +0300

    I forgot destory seo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :commented_posts, through: :comments, source: :post
-  has_one  :seo, as: :seoable
+  has_one  :seo, as: :seoable, dependent: :destroy
 
   validates :name, :email, presence: true
   validates :name, :email, uniqueness: true
