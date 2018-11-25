289f9f501e76b65edfda5eed111d0fa87025858a
traycers <name@mail.com>
2018-11-25 23:34:57 +0300

    fixed default template engine

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Blog
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.generators.tempalte_engie = :slim
+    config.generators.template_engine = :slim
     config.sass.preferred_syntax = :sass
   end
 end
